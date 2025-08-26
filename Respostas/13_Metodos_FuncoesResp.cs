992c1a2df7487b1799aff977de9b33192f3a87be
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-08-26 10:26:49 -0300

    update: 13_Metodos_Funcoes_Resp.cs

--- Respostas/13_Metodos_FuncoesResp.cs
+++ Respostas/13_Metodos_FuncoesResp.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Text;
 
-public class Metodos_Funcoes_Resp
-{
+public class Metodos_Funcoes_Resp{
+    
     public static void Main_Resp13(string[] args)
     {
-
-
-
+        Console.OutputEncoding = Encoding.UTF8;
+        
+        Console.WriteLine("Bem-vindo ao Main");
+        Console.WriteLine("\nDigite um número: ");
+        string? entrada = Console.ReadLine();
+        
+        FuncaoSeparada(entrada);
+        
+        
+        Console.WriteLine("Fim do Main");
+        
     }
+
+    public static void FuncaoSeparada(string entrada){
+        int.TryParse(entrada, out int entradaInt);
+        
+        Console.WriteLine($"O número foi: {entradaInt}");
+    }
+    
 }
