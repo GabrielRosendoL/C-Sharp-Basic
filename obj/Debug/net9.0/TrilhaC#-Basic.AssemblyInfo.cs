761aa23e3bba0e8f51a08c498fc8dee20e886013
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-09-10 11:49:30 -0300

    update: Programa_12 Resp

--- obj/Debug/net9.0/TrilhaC#-Basic.AssemblyInfo.cs
+++ obj/Debug/net9.0/TrilhaC#-Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrilhaC#-Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedf2da4515dfea9687e9105e24fb8735d3e7acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1151736097bddb8a399dfd1c0ad159059587e496")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrilhaC#-Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrilhaC#-Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
